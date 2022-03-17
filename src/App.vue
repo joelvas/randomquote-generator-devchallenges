4ae360f7eb12f2b71f08939ba9be3ee0d275266d
jsonvas <ysonvas@gmail.com>
2022-03-17 18:00:03 -0500

    quotes generator completed

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,76 @@
 <template>
-  <div>Init</div>
+  <div class="px-8 md:px-14">
+    <div class="flex flex-row justify-end pt-4">
+      <button class="flex flex-row items-center text-gray-500" @click="getQuote">
+        <div class="text-[11px]">random</div>
+        <span class="material-icons text-[16px] pl-1">autorenew</span>
+      </button>
+    </div>
+    <div v-if="author !== ''" class="font-bold">{{ author }}</div>
+    <div
+      :class="[
+        'flex flex-row justify-center items-center',
+        'h-full',
+        author !== '' ? ' mt-10' : 'mt-20',
+      ]"
+    >
+      <QuoteCard :quotesData="quotes" @setAuthor="setAuthor" />
+    </div>
+    <div></div>
+  </div>
 </template>
 
 <script>
+import QuoteCard from './components/QuoteCard.vue';
+
+const axios = require('axios');
+
 export default {
+  components: {
+    QuoteCard,
+  },
   name: 'App',
+  data() {
+    return {
+      author: '',
+      count: 1,
+      quotes: [],
+      baseUrl: 'https://inspiring-quotes.p.rapidapi.com/multiple',
+      headers: {
+        'x-rapidapi-key': 'e7efca42d3msh724414ab88b87c9p174415jsn385d1bb520aa',
+        'x-rapidapi-host': 'inspiring-quotes.p.rapidapi.com',
+      },
+    };
+  },
+  mounted() {
+    this.getQuote();
+  },
+  methods: {
+    async getQuote() {
+      this.author = '';
+      this.quotes = [];
+      const res = await axios.get(`${this.baseUrl}/?count= ${this.count}`, {
+        headers: this.headers,
+      });
+      if (res) {
+        this.quotes = res.data.data;
+      }
+    },
+    async getQuotesByAuthor() {
+      this.quotes = [];
+      const res = await axios.get(`${this.baseUrl}/?count=5&author=${this.author}`, {
+        headers: this.headers,
+      });
+      if (res) {
+        this.quotes = res.data.data;
+      }
+    },
+    setAuthor(author) {
+      this.author = author;
+      this.quotes = [];
+      this.getQuotesByAuthor();
+    },
+  },
 };
 </script>
 
@@ -15,6 +81,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/QuoteCard.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div v-for="(item, i) in quotesData" :key="i" class="py-8">
+      <div
+        class="flex flex-col items-center justify-center w-96 border-l-4 border-orange-200 px-10"
+      >
+        <div>
+          {{ `"${item.quote}"` }}
+        </div>
+      </div>
+      <div
+        v-if="quotesData.length === 1"
+        :class="[
+          'px-5 mt-10 py-3 mx-8',
+          'flex flex-row justify-between',
+          'hover:bg-neutral-800 hover:text-white',
+        ]"
+        style="cursor: pointer"
+        @click="setAuthor(item.author)"
+        @keyup="() => {}"
+      >
+        <div>
+          <div class="text-[13px] text-gray-600 font-bold">{{ item.author }}</div>
+          <div class="text-[10px] text-gray-500">{{ item.genre }}</div>
+        </div>
+        <button>
+          <span class="material-icons inline text-white">arrow_right_alt</span>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    quotesData: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+  methods: {
+    setAuthor(author) {
+      this.$emit('setAuthor', author);
+    },
+  },
+};
+</script>
+
+<style></style>
